4886ab34248245b8ff66e7a74bd5fb8fee3f881a
Charan B A <cba649180@gmail.com>
2024-12-27 23:01:49 +0530

    commited

---
+++ src/main/java/com/jsp/servlet/calculator/MultiplicationServlet.java
@@ -0,0 +1,27 @@
+package com.jsp.servlet.calculator;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class MultiplicationServlet extends GenericServlet {
+
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		String n1=req.getParameter("num1");
+		int num1=Integer.parseInt(n1);
+		int num2=Integer.parseInt(req.getParameter("num2"));
+		
+		int mul=num1*num2;
+		
+		PrintWriter out=res.getWriter();
+		out.print("<h1>The mul of the numbers is "+ mul+ "<h1>");
+	}
+	
+}
+
